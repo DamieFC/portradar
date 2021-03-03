76844d7b4a034433dc6fcbadd43f40a28154def5
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-03 12:55:00 -0800

    Create portradar.c
    Help me

---
+++ scanner/portradar.c
@@ -0,0 +1,8 @@
+/************************************************
+ * This file will handle the basics of the port *
+ * scanner, including pinging and printing the  *
+ * results. It is licensed under the Affero GNU *
+ * GPL.                                         *
+ ************************************************/
+
+#include <stdio.h>
